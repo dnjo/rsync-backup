f33fd6b9530e0b5e62bb8b6a4ac2f95017c56b6c
Dennis Johansson <dennis@dnjo.net>
2018-01-21 02:13:58 +0100

    Initial commit

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+set -ef -o pipefail
+
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+while read path; do
+    rsync -av --delete $path $BACKUP_LOCATION
+done < "$dir/backup_list"
